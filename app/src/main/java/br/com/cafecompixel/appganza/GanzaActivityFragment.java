d55b3ed61cae8a556b6cf915e6ac64e0b090db2a
Rafael Malheiros <rafael_malheiros@hotmail.com>
2015-10-28 18:59:14 -0200

    setup de projeto

---
+++ app/src/main/java/br/com/cafecompixel/appganza/GanzaActivityFragment.java
@@ -0,0 +1,22 @@
+package br.com.cafecompixel.appganza;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class GanzaActivityFragment extends Fragment {
+
+    public GanzaActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ganza, container, false);
+    }
+}
