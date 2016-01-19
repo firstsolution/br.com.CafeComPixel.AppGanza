d835850606c1ad027e5d5d32358d74794e28d3c4
Markoshd <marcossilva@firstsolution.com.br>
2016-01-19 11:16:25 -0200

    Adicionar funcao mudar cor na tela modo adulto

--- app/src/main/java/br/com/cafecompixel/appganza/fragment/GanzaFragment.java
+++ app/src/main/java/br/com/cafecompixel/appganza/fragment/GanzaFragment.java
@@ -1,5 +1,6 @@
 package br.com.cafecompixel.appganza.fragment;
 
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
@@ -7,8 +8,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import br.com.cafecompixel.appganza.R;
+import br.com.cafecompixel.appganza.activity.GanzaActivity;
 import br.com.cafecompixel.appganza.util.ShakeDetector;
 import br.com.cafecompixel.appganza.util.SoundManager;
 
@@ -17,9 +20,22 @@ import br.com.cafecompixel.appganza.util.SoundManager;
  */
 public class GanzaFragment extends Fragment {
 
+    public static final String EGG_BLACk  = "egg_black";
+    public static final String EGG_BLUE   = "egg_blue";
+    public static final String EGG_GREEN  = "egg_green";
+    public static final String EGG_RED    = "egg_red";
+    public static final String EGG_PURPLE = "egg_purple";
+    public static final String EGG_PINK   = "egg_pink";
+    public static final String EGG_YELLOW = "egg_yellow";
+
+    private ImageView eggImageView;
+
     private SensorManager mSensorManager;
     private Sensor        mAccelerometer;
     private ShakeDetector mShakeDetector;
+
+    SharedPreferences settings;
+    View layout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,7 +51,10 @@ public class GanzaFragment extends Fragment {
             }
         });
 
-        return inflater.inflate(R.layout.fragment_ganza, container, false);
+        settings =  getActivity(). getSharedPreferences(GanzaActivity.PREFS_NAME, 0);
+        layout = inflater.inflate(R.layout.fragment_ganza, container, false);
+
+        return layout;
 
     }
 
@@ -46,6 +65,47 @@ public class GanzaFragment extends Fragment {
         mSensorManager.registerListener(mShakeDetector, mAccelerometer,
                 SensorManager.SENSOR_DELAY_UI);
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        String color = settings.getString(ConfigGanzaFragment.EGG_COLOR, EGG_PURPLE);
+
+        // ImageView que ira conter a imagem do ovo
+        eggImageView = (ImageView) layout.findViewById(R.id.eggImg);
+
+        switch(color) {
+
+            case EGG_BLACk:
+                eggImageView.setImageResource(R.drawable.egg_black);
+                break;
+
+            case EGG_PINK:
+                eggImageView.setImageResource(R.drawable.egg_pink);
+                break;
+
+            case EGG_RED:
+                eggImageView.setImageResource(R.drawable.egg_red);
+                break;
+
+            case EGG_PURPLE:
+                eggImageView.setImageResource(R.drawable.egg_purple);
+                break;
+
+            case EGG_BLUE:
+                eggImageView.setImageResource(R.drawable.egg_blue);
+                break;
+
+            case EGG_GREEN:
+                eggImageView.setImageResource(R.drawable.egg_gree);
+                break;
+
+            case EGG_YELLOW:
+                eggImageView.setImageResource(R.drawable.egg_yellow);
+
+        }
+    }
+
 
     @Override
     public void onPause() {
